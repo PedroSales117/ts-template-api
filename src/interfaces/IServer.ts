44c8c99fc02c06c37564f58eb98cc586585dc431
PedroSales117 <pedrohenrysalles@outlook.com>
2024-03-20 21:43:32 -0300

    docs(IServer): enhance interface documentation and ensure robust typing
    - Add JSDoc comments for better clarity on the IServer interface methods.
    - Ensure consistency in async operations with Result<void, string> returns.
    
    These enhancements aim to improve code readability, maintainability, and
    the overall developer experience when interacting with the IServer interface.

--- src/interfaces/IServer.ts
+++ src/interfaces/IServer.ts
@@ -1,8 +1,29 @@
 import { Result } from "../helpers/result.helper";
 import { IRouter, IUseCallback } from "./";
 
+/**
+ * Interface for a server that can use plugins, routers, and listen on a port.
+ */
 export interface IServer {
+  /**
+   * Registers a plugin or middleware with an optional prefix.
+   * @param prefix The URL prefix for the middleware.
+   * @param opts The plugin or middleware callback.
+   * @returns A promise that resolves to a Result indicating success or failure.
+   */
   use(prefix: string, opts: IUseCallback): Promise<Result<void, string>>;
+
+  /**
+   * Adds a collection of routers to the server.
+   * @param routers An array of IRouter instances.
+   * @returns A promise that resolves to a Result indicating success or failure.
+   */
   useRouters(routers: IRouter[]): Promise<Result<void, string>>;
+
+  /**
+   * Starts the server to listen on the specified port.
+   * @param port The port number to listen on.
+   * @returns A promise that resolves to a Result indicating success or failure.
+   */
   listen(port: number): Promise<Result<void, string>>;
 }
