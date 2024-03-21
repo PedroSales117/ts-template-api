88db6eacecfa7726c27aa44bff945c2cade1c593
PedroSales117 <pedrohenrysalles@outlook.com>
2024-03-20 22:48:25 -0300

    feat(server-adapter): add JSDoc comments for clarity
    Added detailed JSDoc comments to the ServerAdapter to improve code readability and understanding.

--- src/configurations/adapters/server.adapter.ts
+++ src/configurations/adapters/server.adapter.ts
@@ -2,13 +2,26 @@ import Fastify, { FastifyInstance } from "fastify";
 import { IRouter, IRoute, IUseCallback } from "../../interfaces";
 import { Result, Ok, Err } from "../../helpers/result.helper";
 
+/**
+ * ServerAdapter provides an abstraction layer over the Fastify framework,
+ * allowing for easy setup and management of routes and middlewares.
+ */
 export class ServerAdapter {
   private server: FastifyInstance;
 
+  /**
+   * Initializes the server with Fastify, enabling logging by default.
+   */
   constructor() {
     this.server = Fastify({ logger: true });
   }
 
+  /**
+   * Registers a middleware or a series of middlewares to the server with an optional prefix.
+   * @param prefix The URL prefix for all paths within the middleware.
+   * @param opts Middleware function or an object describing the middleware.
+   * @returns A Result indicating success (Ok) or an error message (Err).
+   */
   async use(prefix: string, opts: IUseCallback): Promise<Result<void, string>> {
     try {
       await this.server.register(opts, { prefix });
@@ -20,6 +33,11 @@ export class ServerAdapter {
     }
   }
 
+  /**
+   * Registers an array of routers to the server, each containing a set of routes.
+   * @param routers An array of IRouter, each router managing multiple routes.
+   * @returns A Result indicating success (Ok) or an error message (Err).
+   */
   async useRouters(routers: IRouter[]): Promise<Result<void, string>> {
     try {
       await this.use("/api", (instance, _opts, done) => {
@@ -42,6 +60,11 @@ export class ServerAdapter {
     }
   }
 
+  /**
+   * Starts the server listening on a specified port.
+   * @param port The port number on which the server should listen.
+   * @returns A Result indicating success (Ok) or an error message (Err).
+   */
   async listen(port: number): Promise<Result<void, string>> {
     try {
       await this.server.listen({ host: "0.0.0.0", port });
