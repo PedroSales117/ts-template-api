439e1f1d2ab691465af6027989524842125c672c
PedroSales117 <pedrohenrysalles@outlook.com>
2024-03-20 22:13:09 -0300

    docs(router): enhance with JSDoc comments for route management methods
    - Introduce JSDoc comments for the Router class methods.
    - Document the prevention of duplicate route additions.
    - Explain the functionality of finding specific routes by path and method.
    
    These comments provide clear guidance on how the Router manages routes,
    making the codebase more accessible to developers.

--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -1,8 +1,16 @@
 import { IRoute, IRouter } from "../interfaces/IRouter";
 
+/**
+ * Implements the IRouter interface, managing routes for the HTTP server.
+ */
 export class Router implements IRouter {
-  private routes: IRoute[] = [];
+  private routes: IRoute[] = []; // Stores the routes added to the router.
 
+  /**
+   * Adds a new route to the router.
+   * Checks if the route already exists to prevent duplicates before adding.
+   * @param route {IRoute} The route to be added.
+   */
   addRoute(route: IRoute): void {
     const routeExists = this.routes.some(
       (existingRoute) =>
@@ -10,19 +18,30 @@ export class Router implements IRouter {
         existingRoute.method === route.method,
     );
 
-    if (routeExists) {
+    if (!routeExists) {
+      this.routes.push(route);
+    } else {
       console.warn(
         `Warning: Route ${route.method} ${route.path} already exists. Duplicate routes are not added.`,
       );
-    } else {
-      this.routes.push(route);
     }
   }
 
+  /**
+   * Returns all the routes added to the router.
+   * @return {IRoute[]} A list of routes.
+   */
   getRoutes(): IRoute[] {
     return this.routes;
   }
 
+  /**
+   * Finds a specific route by path and method.
+   * Useful for debugging or additional functionalities.
+   * @param path {string} The path of the route to be found.
+   * @param method {string} The HTTP method of the route to be found.
+   * @return {IRoute | undefined} The found route or undefined if not found.
+   */
   findRoute(path: string, method: string): IRoute | undefined {
     return this.routes.find(
       (route) => route.path === path && route.method === method,
