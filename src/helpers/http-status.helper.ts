1e92829431e50de45613b0eefa699f7e41b14894
PedroSales117 <pedrohenrysalles@outlook.com>
2024-03-20 22:56:33 -0300

    refactor(http-status): add JSDoc and init HttpStatus mapping
    Introduce JSDoc comments for HttpStatus mapping.

--- src/helpers/http-status.helper.ts
+++ src/helpers/http-status.helper.ts
@@ -1,5 +1,12 @@
 import { StatusCodes } from "http-status-codes";
 
+/**
+ * A mapping object for HTTP status codes to enhance readability and maintainability
+ * throughout the application. It leverages the `http-status-codes` library for
+ * consistent status code references.
+ *
+ * It's intended to be expanded with additional status codes as needed.
+ */
 export const HttpStatus = {
-  OK: StatusCodes.OK,
+  OK: StatusCodes.OK, // Represents a 200 OK HTTP status code.
 };
