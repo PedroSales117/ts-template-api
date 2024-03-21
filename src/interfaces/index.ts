08026739f88fc95ace7b2cf1ffe7ece8a6bb8eac
PedroSales117 <pedrohenrysalles@outlook.com>
2024-03-20 23:15:44 -0300

    docs(module-exports): add comments to export statements
    Explained reexports for IRouter, IServer, IFastifyCallback modules.

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,8 @@
+// Reexports all exports from the IRouter module.
 export * from "./IRouter";
+
+// Reexports all exports from the IServer module.
 export * from "./IServer";
+
+// Reexports all exports from the IFastifyCallback module.
 export * from "./IFastifyCallback";
