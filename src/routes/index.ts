47a0050ab3bf0d1f571ecd420cbbb785f7a9b4e1
PedroSales117 <pedrohenrysalles@outlook.com>
2024-08-04 23:36:24 -0300

    feat(routes): add reexport for generate-report.route
    - Added reexport for generate-report.route in index file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 // Reexports all exports from the Root Route module.
 export * from "./root.route";
+export * from "./generate-report.route";
 
 // Reexports all exports from the Router module.
 export * from "./Router";
