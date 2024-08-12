ab9393ba6b44996d6e805b5654d6aaae7892a917
PedroSales117 <pedrohenrysalles@outlook.com>
2024-08-12 18:14:07 -0300

    refactor(utils): re-export additional `ResultAsync` type from `neverthrow`
    - Added re-export of the `ResultAsync` type from `neverthrow` for handling async results.
    - Updated the module to re-export both `Result` and `ResultAsync` types, along with `Ok` and `Err`.

--- src/helpers/result.helper.ts
+++ src/helpers/result.helper.ts
@@ -1,4 +1,4 @@
-import { Result as R, ok, err } from "neverthrow";
+import { Result as R, ok, err, ResultAsync as RAsync } from "neverthrow";
 
 /**
  * Re-exports the Result type from the `neverthrow` package, enabling typed results with success or error states.
@@ -7,6 +7,15 @@ import { Result as R, ok, err } from "neverthrow";
  * @type {Result<T, E>} The generic Result type, where `T` is the type of the success value and `E` is the type of the error.
  */
 export type Result<T, E> = R<T, E>;
+
+/**
+ * Re-exports the `ResultAsync` type from the `neverthrow` package, which enables asynchronous handling of results
+ * with success or error states. This is useful for dealing with promises that can either resolve successfully or
+ * reject with an error.
+ *
+ * @type {ResultAsync<T, E>} The generic ResultAsync type, where `T` is the type of the success value and `E` is the type of the error.
+ */
+export const ResultAsync = RAsync;
 
 /**
  * Re-exports the `ok` function from the `neverthrow` package, providing a convenient way to create a `Result` instance representing success.
