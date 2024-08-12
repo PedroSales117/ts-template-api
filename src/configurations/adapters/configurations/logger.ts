a54afa9391f939a324fdb4c63cdc757d3098ceed
PedroSales117 <pedrohenrysalles@outlook.com>
2024-08-12 18:14:40 -0300

    chore(logger): configure logger options with `pino-pretty`
    - Configured logger options to use `pino-pretty` for improved readability in development.
    - Set the logging level to `info`.
    - Enabled colorization and time translation in logs.
    - Configured to ignore `pid` and `hostname` fields in the log output.

---
+++ src/configurations/adapters/configurations/logger.ts
@@ -0,0 +1,15 @@
+export const loggerOptions = {
+    level: 'info',
+    transport: {
+        targets: [
+            {
+                target: 'pino-pretty',
+                options: {
+                    colorize: true,
+                    translateTime: 'SYS:standard',
+                    ignore: 'pid,hostname',
+                },
+            },
+        ],
+    },
+}
