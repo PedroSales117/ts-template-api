c49a757474a5639d997dc664a6f53d5be7b36c1c
PedroSales117 <pedrohenrysalles@outlook.com>
2024-08-12 18:27:11 -0300

    refactor(server): enhance error handling and logging in server initialization
    Improved server init by adding better error handling, logging, and process exit on failure.

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,33 @@
 import "dotenv/config";
-import { generateReportRouter, rootRoute } from "./routes";
+import { rootRoute } from "./routes/root.route";
 import { HttpServer } from "./configurations/server";
+import logger from "./utils/logger";
 
 /**
  * Initializes and configures the HTTP server.
- * Adds necessary routes to the server and starts listening on a specified port.
- * The port is defined by the PORT environment variable or uses 3000 as a default.
+ * Adds necessary routes to the server and starts listening on a specified PORT.
+ * The PORT is defined by the PORT environment variable or uses 3000 as a default.
  */
 const app = async (): Promise<void> => {
-  // Creates a new instance of the HTTP server.
-  const server = new HttpServer();
+  try {
+    // Creates a new instance of the HTTP server.
+    const server = new HttpServer();
 
-  // Adds routes to the server. useRouters is now asynchronous.
-  await server.useRouters([rootRoute(), generateReportRouter()]);
+    // Adds routes to the server. useRouters is now asynchronous.
+    await server.useRouters([rootRoute()]);
 
-  // Sets the port with a default value of 3000 if the PORT environment variable is not defined.
-  const port = Number(process.env.PORT) || 3000;
+    // Sets the PORT with a default value of 3000 if the PORT environment variable is not defined.
+    const PORT = process.env.PORT;
 
-  // Attempts to start the server on the specified port and handles errors, if any.
-  const listenResult = await server.listen(port);
-  if (listenResult.isErr()) {
-    console.error(`Error starting server: ${listenResult.error}`);
+    // Attempts to start the server on the specified PORT and handles errors, if any.
+    const listenResult = await server.listen(PORT);
+    if (listenResult.isErr()) {
+      logger.error(`Error starting server: ${listenResult.error}`);
+      process.exit(1); // Exiting the process if the server fails to start
+    }
+  } catch (error) {
+    logger.error(`Unexpected error during server initialization: ${error}`);
+    process.exit(1); // Exiting the process if an unexpected error occurs
   }
 };
 
